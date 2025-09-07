141e479c5c7a326965cc5bc007a59b99a15431b6
Galvedir <jordanc86@gmail.com>
2025-09-07 18:26:16 -0400

    update .env.example

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'energy',
-  database: process.env.DB_NAME || 'circuitsorcery',
+  host: process.env.DB_HOST || '',
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
